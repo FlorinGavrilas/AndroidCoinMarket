0c11a5893339ea1830bddb77bded5fad8e66efd3
Florin Gavrilas <florin.gavrilas95@gmail.com>
2018-08-21 12:01:24 +0300

    update

--- app/src/main/java/com/siveco/fg/androidcoinmarket/APIInterface.java
+++ app/src/main/java/com/siveco/fg/androidcoinmarket/APIInterface.java
@@ -10,7 +10,7 @@ import retrofit2.http.Query;
 public interface APIInterface {
 
     //https://pro.coinmarketcap.com/api/v1#section/Quick-Start-Guide
-    @Headers("X-CMC_PRO_API_KEY: 3069b872-06b0-40f1-bf44-5a3f7f73d796")
+    @Headers("X-CMC_PRO_API_KEY: 3f921709-ae40-4835-8f1b-ce7a88de1f12")
     @GET("/v1/cryptocurrency/listings/latest?")
     Call<CryptoList> doGetUserList(@Query("limit") String page);
 }
